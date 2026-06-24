387e208f1134aeecf18a2a60a7ac0c7e8cae198a
alvespb0 <alvespb0@gmail.com>
2026-06-24 17:17:39 -0300

    feat(): feito funcao de consulta boleto prod e sua respectiva classe de payload

--- app/Bancos/Sicoob/Services/SicoobCobrancaService.php
+++ app/Bancos/Sicoob/Services/SicoobCobrancaService.php
@@ -10,6 +10,7 @@ use App\Models\BoletoCobranca;
 use App\Models\Integracao;
 
 use App\Bancos\Sicoob\Payloads\BoletoPayload;
+use App\Bancos\Sicoob\Payloads\ConsultaBoletoPayload;
 
 class SicoobCobrancaService
 {
@@ -58,6 +59,19 @@ class SicoobCobrancaService
         return $response;
     }
 
+    /**
+     * Registra um boleto na API de cobrança do Sicoob .
+     *
+     * Autentica no fluxo oAuth 2.0 com o escopo boletos_inclusao
+     *
+     * O payload é montado a partir dos dados do boleto,
+     * parcela, conta bancária, configuração de cobrança
+     * e pagador.
+     *
+     * @param BoletoCobranca $boleto Boleto a ser registrado.
+     *
+     * @return array Resposta retornada pela API do Sicoob.
+     */
     public function gerarBoletoProducao(BoletoCobranca $boleto){
         $authService = new AuthService;
         $access_token = $authService->auth($this->integracao, 'boletos_inclusao');
@@ -84,6 +98,28 @@ class SicoobCobrancaService
             );
         
         return $response;
+    }
 
+    public function consultarBoletoProducao(BoletoCobranca $boleto){
+        $authService = new AuthService;
+        $access_token = $authService->auth($this->integracao, 'boletos_consulta');
+        $client_id = $this->integracao->credenciais->client_id;
+        $cert = $this->integracao->empresaParametro->certificadoDigital;
+
+        $payloadMounter = new ConsultaBoletoPayload;
+        $payload = $payloadMounter->payloadMount($boleto);
+
+        \Log::debug(['Payload de consta de boleto produção' => $payload]);
+
+        $response = Http::withToken($access_token)
+            ->withOptions([
+                'cert' => Storage::disk('local')->path($cert->cert_path)
+            ])
+            ->withHeaders([
+                'client_id' => $client_id,
+            ])
+            ->get($this->integracao->endpoint . 'cobranca-bancaria/v3/boletos', $payload);
+
+        return $response;
     }
 }
