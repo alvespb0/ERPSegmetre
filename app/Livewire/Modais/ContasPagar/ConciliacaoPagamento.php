ef425e974715d6dcf819df736eb019e144580392
alvespb0 <alvespb0@gmail.com>
2026-08-19 17:15:30 -0300

    feat(): feito funcao de conciliacao

--- app/Livewire/Modais/ContasPagar/ConciliacaoPagamento.php
+++ app/Livewire/Modais/ContasPagar/ConciliacaoPagamento.php
@@ -4,12 +4,17 @@ namespace App\Livewire\Modais\ContasPagar;
 
 use Livewire\Component;
 
+use App\Helpers\Empresa;
+
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\SolicitacoesPagamento;
 use App\Models\Parcela;
+
+use App\Services\MovimentacaoService;
 
 class ConciliacaoPagamento extends Component
 {
@@ -57,6 +62,78 @@ class ConciliacaoPagamento extends Component
 
     public function fechar(){
         $this->dispatch('fechar-modal-conciliacao');
+    }
+
+    public function rules()
+    {
+        return [
+            'parcela_id' => 'required|exists:parcelas,id',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'parcela_id.required' => 'Selecione uma parcela para vincular o pagamento.',
+            'parcela_id.exists' => 'A parcela selecionada é inválida.',
+        ];
+    }
+
+    public function submit(MovimentacaoService $movimentacaoService){
+        try {
+            $this->validate();
+
+
+            $parcela = Parcela::findOrFail($this->parcela_id);
+
+            if ($parcela->status === 'cancelado') {
+                $this->dispatch('toast-error', 'Não é possível vincular pagamento a parcela cancelada.');
+                return;
+            }
+
+            if ($parcela->possuiSolicitacaoPagamento) {
+                $this->dispatch('toast-error', 'Parcela já possui uma solicitação de pagamento pendente.');
+                return;
+            }
+
+            if ($parcela->saldo_devedor == 0 || $parcela->saldo_devedor < $this->pagamento->valor) {
+                $this->dispatch('toast-error', 'Saldo devedor insuficiente mentor que valor de pagamento.');
+                return;
+            }
+
+            if(!$this->pagamento->conta_id){
+                $this->dispatch('toast-error', 'Pagamento sem conta de origem.');
+                return;
+            }
+
+            DB::beginTransaction();
+
+            $movimentacao = $movimentacaoService->store([
+                'conta_id' => $this->pagamento->conta_id,
+                'empresa_parametro_id' => Empresa::id(),
+                'parcela_id' => $parcela->id,
+                'valor_pago' => $this->pagamento->valor,
+                'data_pagamento' => $this->pagamento->data_pagamento ?? $this->pagamento->data_solicitacao,
+            ]);
+
+            $this->pagamento->update([
+                'movimentacao_id' => $movimentacao->id,
+                'parcela_id' => $parcela->id,
+            ]);
+
+            DB::commit();
+            $this->dispatch('toast-message', 'Pagamento vinculado à despesa existente com sucesso!');
+            $this->fechar();
+        } catch (ValidationException $e) {
+            throw $e;
+        } catch (\Throwable $e) {
+            DB::rollback();
+            \Log::error('Erro ao fazer conciliação de pagamento e parcela', [
+                'erro' => $e->getMessage(),
+            ]);
+            $this->dispatch('toast-error', 'Erro na conciliação.');
+        }
+
     }
 
     public function render()
